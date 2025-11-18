51f42446bfc728b5c60eb797ff8f2bfcef3e57fc
LeoDiasRodrigues <leozezudr@gmail.com>
2025-11-18 16:22:02 -0300

    Add files via upload

--- htdocs/aula_login/cadastro.php
+++ htdocs/aula_login/cadastro.php
@@ -1,3 +1,27 @@
+<?php
+
+include("my_conecction.php");
+
+if ($_POST) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $confirm_password = $_POST['confirm_password'];
+
+    $sql = "INSERT INTO usuario (nome ,email, password) VALUES ('$name','$email', '$password')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Usuario cadastrado com sucesso!";
+    } else {
+        echo "Erro: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,38 +33,41 @@
 </head>
 
 <body>
-    <form action="processo_cadastro.php" method="POST">
-    <div class="fundo">
-        <div class="container flex tudo-centro">
-            <div class="quadrado-inicial flex space-between">
+    <form method="POST">
+        <div class="fundo">
+            <div class="container flex tudo-centro">
+                <div class="quadrado-inicial flex space-between">
 
 
-                <div class="quadrado-entrar flex tudo-centro coluna">
-                    <h1 class="flex tudo-centro tamanho-medio">Bem-vindo de volta</h1>
-                    <h1 class="flex tudo-centro tamanho-pequeno">Acesse sua conta agora</h1>
-                    <br>
-                    <button class="flex tamanho-botao-entrar cor-botao"><a href="login.php">Entrar</a></button>
-                </div>
+                    <div class="quadrado-entrar flex tudo-centro coluna">
+                        <h1 class="flex tudo-centro tamanho-medio">Bem-vindo de volta</h1>
+                        <h1 class="flex tudo-centro tamanho-pequeno">Acesse sua conta agora</h1>
+                        <br>
+                        <button class="flex tamanho-botao-entrar cor-botao"><a href="login.php">Entrar</a></button>
+                    </div>
 
-                <div class="flex quadrado-meio tudo-centro coluna">
-                    <h1 class="flex tamanho-medio">Crie sua conta</h1>
-                    <br>
+                    <div class="flex quadrado-meio tudo-centro coluna">
+                        <h1 class="flex tamanho-medio">Crie sua conta</h1>
+                        <br>
 
-                    <input type="email" placeholder="Email" class="input fonte" name="email">
-                    <br>
+                        <input type="text" placeholder="Nome" class="input fonte" name="name">
+                        <br>
 
-                    <input type="password" placeholder="Senha" class="input fonte" name="password">
+                        <input type="email" placeholder="Email" class="input fonte" name="email">
+                        <br>
 
-                    <br>
-                    <input type="password" placeholder=" Confirme sua senha" class="input fonte" name="confirm_password">
+                        <input type="password" placeholder="Senha" class="input fonte" name="password">
 
-                    <br>
-                    <button class="flex tamanho-cadastrar cor-cadastrar">Cadastrar</button>
+                        <br>
+                        <input type="password" placeholder=" Confirme sua senha" class="input fonte" name="confirm_password">
 
+                        <br>
+                        <button type="submit" class="flex tamanho-cadastrar cor-cadastrar" onclick="cadastrar()">Cadastrar</button>
+
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
     </form>
 </body>
 
--- htdocs/aula_login/login.php
+++ htdocs/aula_login/login.php
@@ -1,53 +1,85 @@
+<?php 
+session_start();
+require "my_conecction.php";
+
+if ($_POST) {
+    $email = $_POST['email'];
+    $password = $_POST['senha'];
+
+    $sql = "SELECT * FROM usuario WHERE email = '$email' LIMIT 1";
+    $res = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($res) > 0) {
+        $user = mysqli_fetch_assoc($res);
+
+        if ($password == $user['senha']) {
+            $_SESSION['id'] = $user['id'];
+            $_SESSION['nome'] = $user['nome'];
+            header("Location: painel.php");
+            exit;
+        } else {
+            $erro = "Senha incorreta!";
+        }
+    } else {
+        $erro = "Usuário não encontrado!";
+    }
+}
+?>
+
+
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="index.css">
 </head>
+
 <body>
-<form action="processo.php" method="POST">
-    <div class="fundo">
-        <div class="container flex tudo-centro">
-            <div class="quadrado-inicial flex space-between">
+    <form method="POST">
+        <div class="fundo">
+            <div class="container flex tudo-centro">
+                <div class="quadrado-inicial flex space-between">
 
 
-                <div class="quadrado-entrar flex tudo-centro coluna"> 
-                    <h1 class="flex tudo-centro tamanho-medio">Novo por aqui?</h1>
-                    <h1 class="flex tudo-centro tamanho-pequeno">Crie sua conta agora:</h1>
-                    <br>
+                    <div class="quadrado-entrar flex tudo-centro coluna">
+                        <h1 class="flex tudo-centro tamanho-medio">Novo por aqui?</h1>
+                        <h1 class="flex tudo-centro tamanho-pequeno">Crie sua conta agora:</h1>
+                        <br>
+                        <div class="flex quadrado-meio tudo-centro coluna">
+
+                            <h1 class="flex tamanho-medio">Crie sua conta</h1>
+
+                            <a href="cadastro.php" class="tamanho-botao-criar flex">Criar</a>
+
+                        </div>
+                    </div>
+
                     <div class="flex quadrado-meio tudo-centro coluna">
+                        <h1 class="flex tamanho-medio">Faça seu login:</h1>
+                        <br>
 
-    <h1 class="flex tamanho-medio">Crie sua conta</h1>
+                        <input type="email" placeholder="Email" class="input fonte" name="email">
+                        <br>
 
-    <a href="cadastro.php" class="tamanho-botao-criar flex">Criar</a>
+                        <input type="password" placeholder="Senha" class="input fonte" name="password">
 
-</div>
-                </div>
-
-                <div class="flex quadrado-meio tudo-centro coluna">
-                    <h1 class="flex tamanho-medio">Faça seu login:</h1>
-                    <br>
-                    
-                    <input type="email" placeholder="Email" class="input fonte" name="email">
-                    <br>
-                    
-                    <input type="password" placeholder="Senha" class="input fonte" name="password">
-
-                    <br>
-                    <button class="flex tamanho-cadastrar cor-cadastrar">Logar</button>
-
-
+                        <br>
+                        <button class="flex tamanho-cadastrar cor-cadastrar">Logar</button>
+                    </div>
 
                 </div>
-
-
-
-                    
             </div>
         </div>
-    </div>
     </form>
 </body>
+
 </html>
